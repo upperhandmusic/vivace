c410f59c576d60dc4820686641b8b6e257c94189
Dave Nunez <david.nunez@alumni.stanford.edu>
2023-01-19 17:10:17 -0800

    feat: export modules from the vivace_core package

---
+++ packages/core/lib/vivace_core.dart
@@ -0,0 +1,9 @@
+/// A library for writing music applications.
+///
+/// The core library focuses on foundational music concepts such as pitch,
+/// scales, intervals, rhythm and more.
+library core;
+
+export 'src/pitch.dart';
+export 'src/pitch_class.dart';
+export 'src/scales.dart';
